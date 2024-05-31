a628cd3e6a7e098eb508bf76187a0ab449fbf6bb
Oleg <moiseev.oleg2@yandex.ru>
2024-05-31 17:58:39 +0300

    Классификатор с готовой моделью и классификатором

---
+++ classification/classification.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import time
+import re
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('C:/Users/79185/Desktop/test_task/datasets/csv_all_in_one/all_stemmer.csv').sample(frac=1)
+df.columns = ['category', 'article_name', 'article_link', 'article_text']
+print(df.head())
+
+X_train, X_test, y_train, y_test = train_test_split(df['article_text'], df['category'], test_size=.15, random_state=42)
+
+
+vectorizer = CountVectorizer()
+
+X_train_BOW = vectorizer.fit_transform(X_train)
+X_test_BOW = vectorizer.transform(X_test)
+
+print(X_train_BOW.shape, X_test_BOW.shape)
+print(X_train.iloc[200])
+
+# Векторное представление
+print(type(X_train_BOW[200]))
+print(X_train_BOW[200])
+
+
+start = time.time()
+
+clf = LogisticRegression(random_state=0, max_iter=1000).fit(X_train_BOW, y_train)
+
+end = time.time() - start
+print(f'Время обучения: {end}')
+
+# вычисляем предсказания
+y_predict_BOW = clf.predict(X_test_BOW)
+
+# вычисляем метрику accuracy
+print(accuracy_score(y_predict_BOW, y_test))
--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from aiogram import Bot, Dispatcher, types
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
 
 TOKEN_API = "7059449160:AAHVxRTEd7HuCAjajZ6j9nGloxvvmdb_rFM"
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher()
 
+# Загрузка обученной модели и инициализация векторизатора
+classifier = joblib.load('my_model.joblib')
+vectorizer = joblib.load('vectorizer.joblib')
 
+
+# Функция для классификации текста
+def classify_text(text):
+    text_tfidf = vectorizer.transform([text])
+    label = classifier.predict(text_tfidf)[0]
+    return label
+
+
+# Обработчик текстовых сообщений
 @dp.message()
-async def echo(message: types.Message):
-    await message.answer(text=message.text)
+async def handle_text(message: types.Message):
+    text = message.text
+    label = classify_text(text)
+    reply_text = f"Тема сообщения: {label}"
+    await message.reply(reply_text)
+
+
+# @dp.message()
+# async def echo(message: types.Message):
+#     await message.answer(text=message.text)
 
 
 if __name__ == '__main__':
